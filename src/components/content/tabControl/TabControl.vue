e6d58e73786a0cc3303c3e026ed4099754da4d44
LionX-S <13141096848@163.com>
2020-03-11 13:03:55 +0800

    '首页开发'

---
+++ src/components/content/tabControl/TabControl.vue
@@ -0,0 +1,55 @@
+<template>
+<div class="tab-control">
+  <div v-for="(item,index) in titles"
+       :key="index"
+       :class="{active:index===currentIndex}"
+       @click="activeClick(index)"
+       class="tab-control-item">
+    <span>{{item}}</span>
+  </div>
+</div>
+</template>
+
+<script>
+  export default {
+    name: "TabControl",
+    data(){
+      return {
+        currentIndex:0
+      }
+    },
+    methods:{
+      activeClick(index){
+        this.currentIndex=index;
+      }
+    },
+    props:{
+      titles:{
+        type:Array,
+        default(){
+          return []
+        }
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .tab-control{
+    display: flex;
+    font-size: 15px;
+  }
+  .tab-control-item{
+    flex: 1;
+    text-align: center;
+    height: 40px;
+    line-height: 40px;
+  }
+  .active{
+    color: var(--color-high-text);
+  }
+  .active span{
+    padding: 5px;
+    border-bottom: 2px solid var(--color-high-text);
+  }
+</style>
--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -5,31 +5,62 @@
     </nav-bar>
     <home-swiper :banner="banner"></home-swiper>
     <recommend-views :recommend="recommend"></recommend-views>
+    <feature-view></feature-view>
+    <tab-control :titles="titles"></tab-control>
+    <ul>
+      <li>礼包1</li>
+      <li>礼包2</li>
+      <li>礼包3</li>
+      <li>礼包4</li>
+      <li>礼包5</li>
+      <li>礼包6</li>
+      <li>礼包7</li>
+      <li>礼包8</li>
+      <li>礼包9</li>
+      <li>礼包10</li>
+      <li>礼包11</li>
+      <li>礼包12</li>
+      <li>礼包13</li>
+      <li>礼包14</li>
+      <li>礼包15</li>
+      <li>礼包16</li>
+      <li>礼包17</li>
+      <li>礼包18</li>
+      <li>礼包19</li>
+      <li>礼包20</li>
+    </ul>
   </div>
 </template>
 
 <script>
   import navBar from "components/common/navbar/navBar";
+  import TabControl from "components/content/tabControl/TabControl";
+
   import {getHomeMultidata} from "network/home";
+
   import homeSwiper from "./homechildren/homeSwiper";
   import RecommendViews from "./homechildren/RecommendViews";
+  import FeatureView from "./homechildren/FeatureView";
 
   export default {
     name: "home",
     components:{
       navBar,
+      TabControl,
       homeSwiper,
-      RecommendViews
+      RecommendViews,
+      FeatureView
+
     },
     data(){
       return {
         banner:[],
-        recommend:[]
+        recommend:[],
+        titles:['流行','新款','精选']
       }
     },
     created() {
       getHomeMultidata().then(res=>{
-        console.log(res);
         this.banner=res.data.banner.list;
         this.recommend=res.data.recommend.list;
       })
@@ -41,5 +72,13 @@
   .home-nav{
     background-color:#FF0036;
     color: #ffffff;
+    position: fixed;
+    top:0;
+    left:0;
+    right: 0;
+    z-index: 9;
+  }
+  #home{
+    padding-top:44px;
   }
 </style>
